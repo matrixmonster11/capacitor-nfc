12809d73f0fd5c0707a249411211b40336a07eb7
Jake <jrunge@precisionplanting.com>
2025-05-21 15:26:55 -0500

    Working foreground dispatch

--- android/src/main/kotlin/com/exxili/capacitornfc/MainActivity.kt
+++ android/src/main/kotlin/com/exxili/capacitornfc/MainActivity.kt
@@ -1,36 +1,11 @@
 package com.exxili.capacitornfc
 
-import android.app.PendingIntent
-import android.content.IntentFilter
-import android.nfc.NfcAdapter
 import android.os.Bundle
 import com.getcapacitor.BridgeActivity
 
 class MainActivity : BridgeActivity() {
-    private var pendingIntent: PendingIntent? = null
-    private val intentFilter: IntentFilter = IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED).apply {
-        try {
-            addDataType("text/plain")
-        }
-        catch (e: IntentFilter.MalformedMimeTypeException) {
-            throw RuntimeException("failed", e)
-        }
-    }
-
     public override fun onCreate(savedInstanceState: Bundle?) {
-        registerPlugin(NFCPlugin::class.java)
         super.onCreate(savedInstanceState)
-
-        pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_MUTABLE)
-    }
-
-    override fun onPause() {
-        super.onPause()
-        NfcAdapter.getDefaultAdapter(this).disableForegroundDispatch(this)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        NfcAdapter.getDefaultAdapter(this).enableForegroundDispatch(this, pendingIntent, arrayOf(intentFilter), null)
+        registerPlugin(NFCPlugin::class.java)
     }
 }
--- android/src/main/kotlin/com/exxili/capacitornfc/NFCPlugin.kt
+++ android/src/main/kotlin/com/exxili/capacitornfc/NFCPlugin.kt
@@ -1,10 +1,23 @@
 package com.exxili.capacitornfc
 
+import android.app.PendingIntent
 import android.content.Intent
 import android.content.IntentFilter
 import android.nfc.NfcAdapter
 import android.nfc.NdefMessage
+import android.nfc.NfcAdapter.*
+import android.nfc.tech.IsoDep
+import android.nfc.tech.MifareClassic
+import android.nfc.tech.MifareUltralight
+import android.nfc.tech.Ndef
+import android.nfc.tech.NdefFormatable
+import android.nfc.tech.NfcA
+import android.nfc.tech.NfcB
+import android.nfc.tech.NfcBarcode
+import android.nfc.tech.NfcF
+import android.nfc.tech.NfcV
 import com.getcapacitor.JSObject
+import com.getcapacitor.Logger
 import com.getcapacitor.Plugin
 import com.getcapacitor.PluginCall
 import com.getcapacitor.PluginMethod
@@ -13,37 +26,33 @@ import java.io.Serializable
 
 @CapacitorPlugin(name = "NFC")
 class NFCPlugin : Plugin() {
-    private var intent: Intent? = null
-    private var ndefFilter: IntentFilter? = null
+    private val techListsArray = arrayOf(arrayOf<String>(
+        IsoDep::class.java.name,
+        MifareClassic::class.java.name,
+        MifareUltralight::class.java.name,
+        Ndef::class.java.name,
+        NdefFormatable::class.java.name,
+        NfcBarcode::class.java.name,
+        NfcA::class.java.name,
+        NfcB::class.java.name,
+        NfcF::class.java.name,
+        NfcV::class.java.name
+    ))
 
-    override fun load() {
-        super.load()
+    public override fun handleOnNewIntent(intent: Intent?) {
+        Logger.info("HANDLING INTENT INTERNAL")
 
-        intent = Intent(this.activity, javaClass).apply {
-            addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
-        }
-
-        ndefFilter = IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED).apply {
-            try {
-                addDataType("text/plain")
-            } catch (e: IntentFilter.MalformedMimeTypeException) {
-                throw RuntimeException("fail", e)
-            }
-        }
-    }
-
-    override fun handleOnNewIntent(intent: Intent?) {
         super.handleOnNewIntent(intent)
 
         if (intent?.action.isNullOrBlank()) {
             return
         }
 
-        if (NfcAdapter.ACTION_NDEF_DISCOVERED == intent?.action) {
+        if (ACTION_NDEF_DISCOVERED == intent?.action) {
             val jsResponse = JSObject()
 
             val ndefMessages: MutableList<Map<String, Any>> = mutableListOf()
-            intent.getParcelableArrayExtra(null, NfcAdapter.EXTRA_NDEF_MESSAGES.javaClass)?.also { rawMessages ->
+            intent.getParcelableArrayExtra(null, EXTRA_NDEF_MESSAGES.javaClass)?.also { rawMessages ->
                 for(message in rawMessages.map { it as NdefMessage }) {
                     val ndefRecords: MutableList<Map<String, Serializable?>> = mutableListOf()
                     for(record in message.records) {
@@ -74,6 +83,32 @@ class NFCPlugin : Plugin() {
     fun writeNDEF(call: PluginCall) {
         print("writeNDEF called")
         call.resolve()
+    }
+
+    override fun handleOnPause() {
+        super.handleOnPause()
+        getDefaultAdapter(this.activity).disableForegroundDispatch(this.activity)
+    }
+
+    override fun handleOnResume() {
+        super.handleOnResume()
+
+        val intent = Intent(context, this.activity.javaClass).apply {
+            addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        }
+
+        val pendingIntent = PendingIntent.getActivity(this.activity, 0, intent, PendingIntent.FLAG_MUTABLE)
+
+        val intentFilter: Array<IntentFilter> = arrayOf(IntentFilter(ACTION_NDEF_DISCOVERED).apply {
+            try {
+                addDataType("text/plain")
+            }
+            catch (e: IntentFilter.MalformedMimeTypeException) {
+                throw RuntimeException("failed", e)
+            }
+        })
+
+        getDefaultAdapter(this.activity).enableForegroundDispatch(this.activity, pendingIntent, intentFilter, techListsArray)
     }
 
     private fun ByteArray.toHexString(): String {
