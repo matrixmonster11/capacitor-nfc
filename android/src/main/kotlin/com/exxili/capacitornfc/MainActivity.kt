b641024631855ccebc25c544841ce1ab04104b6d
Jake <jrunge@precisionplanting.com>
2025-05-21 12:14:53 -0500

    Attempting foreground dispatch

--- android/src/main/kotlin/com/exxili/capacitornfc/MainActivity.kt
+++ android/src/main/kotlin/com/exxili/capacitornfc/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.exxili.capacitornfc
 
+import android.nfc.NfcAdapter
 import android.os.Bundle
+import android.util.Log
 import com.getcapacitor.BridgeActivity
 
 class MainActivity : BridgeActivity() {
     public override fun onCreate(savedInstanceState: Bundle?) {
         registerPlugin(NFCPlugin::class.java)
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        NfcAdapter.getDefaultAdapter(this)?.let {
+            if (it.isEnabled) { // Check if NFC is enabled before disabling
+                it.disableForegroundDispatch(this)
+                Log.d("NFC", "Foreground dispatch disabled.")
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        NfcAdapter.getDefaultAdapter(this)?.let {
+            if (it.isEnabled) { // Check if NFC is enabled on the device
+                it.enableForegroundDispatch(this, null, null, null)
+                Log.d("NFC", "Foreground dispatch enabled.")
+            } else {
+                Log.w("NFC", "NFC is disabled. Cannot enable foreground dispatch.")
+                // Optionally prompt user to enable NFC
+            }
+        }
     }
 }
--- android/src/main/kotlin/com/exxili/capacitornfc/NFCPlugin.kt
+++ android/src/main/kotlin/com/exxili/capacitornfc/NFCPlugin.kt
@@ -1,5 +1,8 @@
 package com.exxili.capacitornfc
 
+import android.content.Intent
+import android.nfc.NfcAdapter
+import android.nfc.NdefMessage
 import com.getcapacitor.JSObject
 import com.getcapacitor.Plugin
 import com.getcapacitor.PluginCall
@@ -8,18 +11,46 @@ import com.getcapacitor.annotation.CapacitorPlugin
 
 @CapacitorPlugin(name = "NFC")
 class NFCPlugin : Plugin() {
-    var identifier: String = "NFCPlugin"
-    var jsName: String = "NFC"
+    override fun handleOnNewIntent(intent: Intent?) {
+        super.handleOnNewIntent(intent)
+
+        if (intent?.action.isNullOrBlank()) {
+            return
+        }
+
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED == intent?.action) {
+            val ndefMessages: MutableList<Map<String, Any>> = mutableListOf()
+            intent.getParcelableArrayExtra(null, NfcAdapter.EXTRA_NDEF_MESSAGES.javaClass)?.also { rawMessages ->
+                for(message in rawMessages.map { it as NdefMessage }) {
+                    val ndefRecords: MutableList<Map<String, Any>> = mutableListOf()
+                    for(record in message.records) {
+                        ndefRecords.add(mapOf(
+                           "type" to "TEXT",
+                           "payload" to record.payload
+                        ))
+                    }
+
+                    ndefMessages.add(mapOf(
+                        "records" to ndefRecords
+                    ))
+                }
+            }
+
+            val jsResponse = JSObject()
+            jsResponse.put("messages", ndefMessages)
+            this.notifyListeners("nfcTag", jsResponse)
+        }
+    }
 
     @PluginMethod
     fun startScan(call: PluginCall) {
-        val ret = JSObject()
-        ret.put("error", "Android NFC scanning does not require 'startScan' method.")
-        call.resolve(ret)
+        print("startScan called")
+        call.reject("Android NFC scanning does not require 'startScan' method.")
     }
 
     @PluginMethod
     fun writeNDEF(call: PluginCall) {
+        print("writeNDEF called")
         call.resolve()
     }
 }
