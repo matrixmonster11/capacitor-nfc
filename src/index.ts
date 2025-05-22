ff400628dbae348120691575b4f64ac9fdf9e36e
Jake <jrunge@precisionplanting.com>
2025-05-22 10:48:10 -0500

    Allow for empty records in payload casts

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,13 @@ export * from './definitions';
 /**
  * Get the payload of an NDEF record as a Uint8Array.
  */
-export const u8Payload = (record: NDEFRecord): Uint8Array => {
-  return new Uint8Array(record.payload ?? []);
+export const u8Payload = (record?: NDEFRecord): Uint8Array => {
+  return new Uint8Array(record?.payload ?? []);
 }
 
 /**
  * Get the payload of an NDEF record as a string.
  */
-export const strPayload = (record: NDEFRecord): string => {
+export const strPayload = (record?: NDEFRecord): string => {
   return new TextDecoder().decode(u8Payload(record));
 }
