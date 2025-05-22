44e305cd51935f0265c738154a7c76ffa05ce01b
Jake <jrunge@precisionplanting.com>
2025-05-22 10:40:37 -0500

    Add convenience functions for getting Uint8Array payload, string payload

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
 import { registerPlugin } from '@capacitor/core';
-import type { NFCPlugin } from './definitions';
 
-const NFC = registerPlugin<NFCPlugin>('NFC');
+import type { NDEFRecord, NFCPlugin } from './definitions';
 
+export const NFC = registerPlugin<NFCPlugin>('NFC');
 export * from './definitions';
-export { NFC };
+
+/**
+ * Get the payload of an NDEF record as a Uint8Array.
+ */
+export const u8Payload = (record: NDEFRecord): Uint8Array => {
+  return new Uint8Array(record.payload ?? []);
+}
+
+/**
+ * Get the payload of an NDEF record as a string.
+ */
+export const strPayload = (record: NDEFRecord): string => {
+  return new TextDecoder().decode(u8Payload(record));
+}
